aadbbd2f26f4e121626cb8ae91e2db4a795e8c71
Enes <enestasbasi@outlook.com>
2019-11-09 14:39:10 -0500

    Remove mutation from Basics.js

--- src/pages/Profile/subpages/Settings/Basics.js
+++ src/pages/Profile/subpages/Settings/Basics.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
-import { Mutation } from "react-apollo";
 import ValidationMutation from "../../../../components/common/ValidationMutation/ValidationMutation";
 
 import { UPDATE_BASIC_INFO } from "../../../../GraphQL";
