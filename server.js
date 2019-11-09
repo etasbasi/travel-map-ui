3be55c3946c09c2244d3758887798b0a9e9b27d8
Enes <enestasbasi@outlook.com>
2019-11-09 14:19:00 -0500

    Change package.json and move npm run build to a different npm command

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const favicon = require("express-favicon");
 const path = require("path");
 
-const port = process.env.PORT || 3000;
 const app = express();
 app.use(favicon(__dirname + "/build/icon.PNG"));
 // the __dirname is the current directory from where the script is running
@@ -14,4 +13,8 @@ app.get("/ping", function(req, res) {
 app.get("/*", function(req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
-app.listen(port, () => console.log(`Server running on port ${port}`));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
+});
